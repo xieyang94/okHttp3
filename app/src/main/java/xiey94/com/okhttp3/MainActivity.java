54e35156ac331858787944270677a1f73b9829b2
xiey <1192171830@qq.com>
2018-10-17 13:20:26 +0800

    first commit

---
+++ app/src/main/java/xiey94/com/okhttp3/MainActivity.java
@@ -0,0 +1,91 @@
+package xiey94.com.okhttp3;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void get() throws IOException {
+        OkHttpClient okHttpClient0 = new OkHttpClient();
+        OkHttpClient okHttpClient1 = new OkHttpClient.Builder().build();
+
+        Request request = new Request.Builder()
+                .url("url")
+                .build();
+
+        Call call = okHttpClient0.newCall(request);
+
+        Response response = call.execute();
+
+    }
+
+    public void getPerson(View view) {
+        Uri uri = Uri.parse("content://contacts/people");
+        Intent intent = new Intent(Intent.ACTION_PICK, uri);
+        startActivityForResult(intent, 0);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 0:
+                if (data == null) {
+                    return;
+                }
+                //处理返回的data,获取选择的联系人信息
+                Uri uri = data.getData();
+                String[] contacts = getPhoneContacts(uri);
+                Log.i("cctw", "" + contacts[0] + ":" + contacts[0]);
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private String[] getPhoneContacts(Uri uri) {
+        String[] contact = new String[2];
+        //得到ContentResolver对象
+        ContentResolver cr = getContentResolver();
+        //取得电话本中开始一项的光标
+        Cursor cursor = cr.query(uri, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            //取得联系人姓名
+            int nameFieldColumnIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+            contact[0] = cursor.getString(nameFieldColumnIndex);
+            //取得电话号码
+            String ContactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor phone = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + ContactId, null, null);
+            if (phone != null) {
+                phone.moveToFirst();
+                contact[1] = phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            }
+            phone.close();
+            cursor.close();
+        } else {
+            return null;
+        }
+        return contact;
+    }
+
+}
